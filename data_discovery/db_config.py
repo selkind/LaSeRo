ecac41bf91a3ca443ab9a65a5bc2df3611a9779c
Sam <samuel.elkind@gmail.com>
2019-09-30 07:33:30 -0700

    scene_ids load automated

--- data_discovery/db_config.py
+++ data_discovery/db_config.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 class LandsatDBCreator:
-    SCENE_ID_TABLE = "CREATE TABLE scene_ids(scene text PRIMARY KEY," \
+    SCENE_ID_TABLE = "CREATE TABLE scene_ids(id integer PRIMARY KEY AUTOINCREMENT, scene text," \
                      " WRS_Path integer, WRS_Row integer, Date DATE, Time TIME, Solar_Azimuth FLOAT, " \
                      "Solar_Elev FLOAT, Cloud_Cover FLOAT)"
 
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     db_manager = LandsatDBCreator(db_dir)
     db_manager.initialize_connection()
     db_manager.initalize_cursor()
-    # db_manager.create_table(db_manager.SCENE_ID_TABLE)
+    db_manager.create_table(db_manager.SCENE_ID_TABLE)
     db_manager.cursor.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
     table_query = db_manager.cursor.fetchall()
     for row in table_query:
--- data_discovery/file_loader.py
+++ data_discovery/file_loader.py
@@ -2,44 +2,45 @@ import csv
 from data_discovery.db_config import LandsatDBCreator
 
 
-def load_scene_id_file(path):
-    data = []
+def load_scene_id_file(path, delimiter):
     with open(path, "r") as f:
-        reader = csv.reader(f, delimiter='\t')
+        reader = csv.reader(f, delimiter=delimiter)
         data = [i for i in reader if i[0] != ""]
     return data
 
 
-def format_value(row):
-    return "({},{},{},{},{},{},{},{}),".format(row[0],row[1],row[2],row[3],row[4],row[5],
-                                              row[6],row[7])
+"""
+Takes array of data loaded from Burton-Johnson scene id file
+available in the supplementary material
+download here: https://www.the-cryosphere.net/10/1665/2016/
+@:param db_manager LandsatDBCreator instance with table scene_ids
+table created
+@:param data list Data loaded from file
+@:returns void
+
+"""
 
 
 def populate_scene_id_table(db_manager, data):
     query = """
     INSERT INTO scene_ids 
                 (scene, WRS_Path, WRS_Row, Date, TIME, Solar_Azimuth, Solar_Elev, Cloud_Cover)
-            VALUES
+            VALUES (? ?,?,?,?,?,?,?)
             """
     for i in data:
-        query += format_value(i) + "\n"
-    query = query[:-2] + ";"
-    db_manager.cursor.execute(query)
-    db_manager.commit()
+        db_manager.cursor.execute(query, i)
+    db_manager.con.commit()
 
 
 if __name__ == "__main__":
     scene_id_path = "/home/dsa/DSA/tc-10-1665-2016-supplement/Supplementary Material/Burton_Johnson_Tile_IDs.txt"
     db_dir = "/home/dsa/DSA/db/Image_IDs.db"
-    db_manager = LandsatDBCreator(db_dir)
-    db_manager.initialize_connection()
-    db_manager.initalize_cursor()
+    scene_db_manager = LandsatDBCreator(db_dir)
+    scene_db_manager.initialize_connection()
+    scene_db_manager.initalize_cursor()
 
-    scenes = load_scene_id_file(scene_id_path)
-    populate_scene_id_table(db_manager, scenes)
+    scenes = load_scene_id_file(scene_id_path, '\t')
 
-    db_manager.cursor.execute("SELECT * FROM scene_ids")
-    results = db_manager.cursor.fetchall()
-    for i in results:
-        print(i)
+    # omit header line from insert statement
+    populate_scene_id_table(scene_db_manager, scenes[1:])
 
