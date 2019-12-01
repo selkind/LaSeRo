9bb8b8f17cb3ecaabbbb27df72646126f7bade67
Sam <samuel.elkind@gmail.com>
2019-12-01 09:41:31 -0800

    stack and chunk function added to raster_tools

--- data_preprocessing/utils/raster_tools.py
+++ data_preprocessing/utils/raster_tools.py
@@ -6,6 +6,7 @@ import rasterio.windows as wnd
 import rasterio.mask
 from rasterio import features
 import fiona
+from progress.bar import FillingSquaresBar
 
 
 """
@@ -55,3 +56,51 @@ def rasterize_label(raster_path, vector_path):
         output = np.expand_dims(output, axis=0)
     return output, meta
 
+
+"""
+For a single scene: Create band stacks, load labels, break stacks and labels into chunks of N X M pixels.
+Saves output as pickled numpy arrays. 
+"""
+def make_stacked_chunks(data_manager, scene_id, chunk_width=512, chunk_height=512):
+    label_path = os.path.join(data_manager.label_dir, i + "_label.TIF")
+    with rio.open(label_path) as label_file:
+        label = label_file.read(1)
+
+    band_rasters = []
+    for b in bands:
+        band_path = os.path.join(data_manager.raw_image_dir, i, "{}_B{}.TIF".format(i, b))
+        with rio.open(band_path) as band_file:
+            band_rasters.append(band_file.read(1))
+
+    band_stack = np.stack(band_rasters, axis=0).transpose(1,2,0)
+    band_rasters.clear()
+
+    scene_height = band_stack.shape[0]
+    scene_width = band_stack.shape[1]
+
+    vertical_chunks = scene_height // chunk_height
+    horizontal_chunks = scene_width // chunk_width
+
+    scene_chunk_dir = os.path.join(data_manager.stack_dir, i)
+    if not os.path.exists(scene_chunk_dir):
+        os.mkdir(scene_chunk_dir)
+
+    with FillingSquaresBar('Processing', max=vertical_chunks * horizontal_chunks) as bar:
+        for j in range(vertical_chunks):
+            for k in range(horizontal_chunks):
+                row_index = j * chunk_height
+                col_index = k * chunk_width
+
+                band_chunk = band_stack[row_index: row_index + chunk_height, col_index: col_index + chunk_width, :]
+                label_chunk = label[row_index: row_index + chunk_height, col_index: col_index + chunk_width]
+
+                data_pixels = np.where(band_chunk > 0)
+                data_pixel_count = np.sum(data_pixels)
+
+                band_chunk_path = os.path.join(scene_chunk_dir, "chunk_{}_{}.npy".format(j, k))
+                label_chunk_path = os.path.join(scene_chunk_dir, "chunk_{}_{}_label.npy".format(j, k))
+
+                if data_pixel_count > 0:
+                    np.save(band_chunk_path, band_chunk, allow_pickle=True)
+                    np.save(label_chunk_path, label_chunk, allow_pickle=True)
+                bar.next()
