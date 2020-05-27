4c7feea30821cc6a250374510dfb8aa5bda4ef5a
Sam <samuel.elkind@gmail.com>
2020-05-27 18:55:27 -0400

    create_session_paths handles creating dirs with depth > 1

--- utils/training.py
+++ utils/training.py
@@ -53,16 +53,14 @@ def create_session_paths(session_name, base_dir='/content/drive/My Drive'):
     session_dir = os.path.join(models_dir, session_name)
 
     if not os.path.isdir(session_dir):
-        os.mkdir(session_dir)
+        os.makedirs(session_dir)
     
-    model = os.path.join(session_dir, f'{session_name}.h5')
     history = os.path.join(session_dir, 'history.json')
     training_log = os.path.join(session_dir, 'logs.csv')
     training_config = os.path.join(session_dir, 'config.json')
     classification_report = os.path.join(session_dir, 'classification_report.txt')
 
-    return {'model': model,
-            'history': history,
+    return {'history': history,
             'logs': training_log,
             'config': training_config,
             'classification_report': classification_report}
