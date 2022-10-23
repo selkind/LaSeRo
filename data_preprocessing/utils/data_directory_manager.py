6f9fba47a818961238fe946bf699d0392f0fd75c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 00:04:06 +0000

    Adding tarfile member sanitization to extractall()

--- data_preprocessing/utils/data_directory_manager.py
+++ data_preprocessing/utils/data_directory_manager.py
@@ -230,5 +230,24 @@ class DataDirectoryManager:
             if not os.path.exists(raw_scene_dir):
                 os.mkdir(raw_scene_dir)
             with tarfile.open(scene_tar) as tar:
-                tar.extractall(raw_scene_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, raw_scene_dir)
 
